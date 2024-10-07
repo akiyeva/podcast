1eac6aea66688ddf8db0792fcd759c082b583ada
thestarrynight17 <thestarrynight17@users.noreply.github.com>
2024-10-07 18:39:22 +0400

    Create topic added

--- Podcast.BLL/Services/TopicManager.cs
+++ Podcast.BLL/Services/TopicManager.cs
@@ -1,14 +1,83 @@
 ﻿using AutoMapper;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore.Query;
+using Podcast.BLL.Helpers;
 using Podcast.BLL.Services.Contracts;
+using Podcast.BLL.ViewModels;
+using Podcast.BLL.ViewModels.SpeakerViewModels;
 using Podcast.BLL.ViewModels.TopicViewModels;
 using Podcast.DAL.DataContext.Entities;
 using Podcast.DAL.Repositories.Contracts;
+using System.Linq.Expressions;
 
 namespace Podcast.BLL.Services;
 
 public class TopicManager : CrudManager<Topic, TopicViewModel, TopicCreateViewModel, TopicUpdateViewModel>, ITopicService
 {
-    public TopicManager(IRepositoryAsync<Topic> repository, IMapper mapper) : base(repository, mapper)
+    private readonly IRepositoryAsync<Topic> _repository;
+    private readonly IMapper _mapper;
+    private readonly IWebHostEnvironment _webHostEnvironment;
+    private readonly string FOLDER_PATH = "";
+    public TopicManager(IRepositoryAsync<Topic> repository, IWebHostEnvironment webHostEnvironment, IMapper mapper) : base(repository, mapper)
     {
+        _repository = repository;
+        _mapper = mapper;
+        _webHostEnvironment = webHostEnvironment;
+        FOLDER_PATH = Path.Combine(_webHostEnvironment.WebRootPath, "admin", "images", "topics");
+    }
+    public async Task<TopicViewModel?> GetAsync(int id)
+    {
+        var topic = await _repository.GetAsync(id);
+        var topicViewModel = _mapper.Map<TopicViewModel>(topic);
+        return topicViewModel;
+    }
+    public override Task<TopicViewModel?> GetAsync(Expression<Func<Topic, bool>> predicate, Func<IQueryable<Topic>, IIncludableQueryable<Topic, object>>? include = null, Func<IQueryable<Topic>, IOrderedQueryable<Topic>>? orderBy = null)
+    {
+        return base.GetAsync(predicate, include, orderBy);
+    }
+
+
+    public override async Task<TopicViewModel> CreateAsync(TopicCreateViewModel createViewModel)
+    {
+        if (createViewModel.CoverFile is null) throw new Exception();
+        if (createViewModel.Name is null) throw new Exception();
+
+        #region FileValidations
+        if (!createViewModel.CoverFile.CheckType()) throw new Exception();  
+        if (!createViewModel.CoverFile.CheckSize(2)) throw new Exception();
+        #endregion
+
+        createViewModel.CoverUrl = await createViewModel.CoverFile.CreateImageAsync(FOLDER_PATH);
+
+        var topic = _mapper.Map<Topic>(createViewModel);
+        var createdTopic = await _repository.CreateAsync(topic);
+
+        var topicViewModel = _mapper.Map<TopicViewModel>(createdTopic);
+        return topicViewModel;
+    }
+
+
+    public override async Task<TopicViewModel> UpdateAsync(TopicUpdateViewModel updateViewModel)
+    {
+        var topic = _mapper.Map<Topic>(updateViewModel);
+
+        var updatedTopic = await _repository.UpdateAsync(topic);
+
+        var topicViewModel = _mapper.Map<TopicViewModel>(updatedTopic);
+
+        return topicViewModel;
+    }
+
+    public override async Task<TopicViewModel> RemoveAsync(int id)
+    {
+        var topic = await _repository.GetAsync(id);
+
+        if (topic == null) throw new Exception("Not found");
+
+        var deletedTopic = await _repository.RemoveAsync(topic);
+
+        var topicViewModel = _mapper.Map<TopicViewModel>(deletedTopic);
+
+        return topicViewModel;
     }
 }
---
+++ Podcast.MVC/Areas/AdminPanel/Controllers/TopicController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Podcast.BLL.Services.Contracts;
+using Podcast.BLL.ViewModels.TopicViewModels;
+using Podcast.DAL.DataContext;
+using Podcast.DAL.DataContext.Entities;
+using Podcast.MVC.Helpers;
+
+namespace Podcast.MVC.Areas.AdminPanel.Controllers
+{
+    public class TopicController : AdminController
+    {
+        private readonly ITopicService _topicService;
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        private readonly string FOLDER_PATH = "";
+        private readonly AppDbContext _appDbContext;
+        public TopicController(ITopicService topicService, IWebHostEnvironment webHostEnvironment, AppDbContext appDbContext)
+        {
+            _topicService = topicService;
+            _webHostEnvironment = webHostEnvironment;
+            FOLDER_PATH = Path.Combine(_webHostEnvironment.WebRootPath, "admin", "images", "topics");
+            _appDbContext = appDbContext;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var topicList = await _topicService.GetListAsync(include: x => x.Include(y => y.Episodes!));
+            return View(topicList);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(TopicCreateViewModel createViewModel)
+        {
+            try
+            {
+                await _topicService.CreateAsync(createViewModel);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+            return RedirectToAction("Index");
+        }
+    }
+}
